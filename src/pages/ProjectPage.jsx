ee28f5c71aade0e24e65a0937e7604a21b759d79
Shana2002 <hansakaravi02@gmail.com>
2024-12-06 20:25:19 +0530

    back icon add

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { PROJECTS } from "../constants";
+import { IoIosArrowBack } from "react-icons/io";
 import project from '../components/Project1';
 
 
@@ -9,8 +10,8 @@ const ProjectPage = () => {
     const project = PROJECTS.find((project)=>project.name === name);
     if(!project) return (<h1>Error</h1>)
   return (
-    <div className='flex bg-black-rgba w-full h-full py-12 px-6 gap-5 rounded-xl relative'>
-        <a className='absolute top-6 left-6 text-4xl cursor-pointer border-2 border-white p-2 text-center corner' >&times;</a>
+    <div className='flex bg-black-rgba w-full h-full pt-28 py-12 px-6 gap-5 rounded-xl relative'>
+        <a className='absolute top-6 left-6 text-4xl cursor-pointer border-2 border-white p-2 text-center rounded-full' ><IoIosArrowBack/></a>
       <img src={project.image} alt="" className='w-1/2 h-[50vh] object-contain' />
       <div className='flex flex-col justify-start'>
         <h3 className='text-4xl py-4'>{project.title}</h3>
